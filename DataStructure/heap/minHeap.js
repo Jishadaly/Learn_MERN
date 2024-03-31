f2a158600d1e2a55aa9f5bb0bbb32f4924dadef4
jishadaly <jishadlm10@gmail.com>
2024-03-31 17:43:22 +0530

    trie

--- DataStructure/heap/minHeap.js
+++ DataStructure/heap/minHeap.js
@@ -34,6 +34,66 @@ class minHeap {
         this.heapfyUp(parentIndex)
      }
   }
+
+  remove(){
+    if (this.heap.length === 0) {
+      return null
+    }
+
+    if (this.heap.length === 1) {
+      return this.heap.pop()
+    }
+
+    const min = this.heap[0]
+    this.heap[0] = this.heap.pop()
+    this.heapfyUp(0)
+
+    return min
+
+  }
+
+  heapfyDown(index){
+    const leftachildIndex = this.getLeftachildIndexi(index)
+    const rightChildIndex =  this.getRightChildIndex(index)
+
+    const minIndex = index
+
+    if(leftachildIndex < this.heap.length && this.heap[leftachildIndex] < this.heap[minIndex]){
+      minIndex = leftachildIndex
+    }
+
+    if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[minIndex]) {
+      minIndex = rightChildIndex
+    }
+
+    if (minIndex !== index) {
+      this.swap(index , minIndex)
+      this.heapfyDown(minIndex)
+    }
+  }
+
+  minHeapSort(){
+    const sortedArr =  []
+  
+    while(this.heap.length > 0){
+      sortedArr.push(this.remove())
+    }
+  
+    return sortedArr
+  }
+  
   
 
+
 }
+
+const min = new minHeap()
+min.insert(28)
+min.insert(281123)
+min.insert(281)
+min.insert(223)
+min.remove()
+
+console.log(min.heap);
+
+
