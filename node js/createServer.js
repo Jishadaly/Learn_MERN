9ae972e6c615fec6e4778a04c92fa87cd3f7ed2b
jishadaly <jishadlm10@gmail.com>
2024-04-05 06:30:52 +0530

    dsa added with more functionality

--- node js/createServer.js
+++ node js/createServer.js
@@ -14,15 +14,51 @@
 //   console.log('server running 3000');
 // })
 
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req , res)=> {
-  res.setHeader('Content-Type' , 'text/plain')
-  res.setHeader('cache-Control' , 'no-cache')
+// const server = http.createServer((req , res)=> {
+//   res.setHeader('Content-Type' , 'text/plain')
+//   res.setHeader('cache-Control' , 'no-cache')
 
-  res.end('hell0oo')
+//   res.end('hell0oo')
+// })
+
+// server.listen(3000,()=>{
+//   console.log("serverrrrrr");
+// })
+
+// const http = require('http')
+
+// const server = http.createServer((req, res)=>{
+//    res.setHeader('Content-Type' , 'text/plain')
+//    res.setHeader('cache-Control' , 'no-cache')
+//   res.end("hello")
+// })
+
+// server.listen(3000,()=>{
+//   console.log('runnimg');
+// })
+
+const express = require('express')
+const app =  express()
+const port =  3000
+
+
+const authMid = (req,res,next) =>{
+  if (1+1 ===1) {
+    next()
+  }else{
+    res.status(401).send("unautherized")
+  }
+}
+app.get('/' , (req,res)=>{
+  res.send("helo world")
 })
 
-server.listen(3000,()=>{
-  console.log("serverrrrrr");
+app.get('/login',authMid ,(req,res)=>{
+  res.send("login page")
 })
+
+ app.listen(port , ()=>{
+  console.log("server is runining 3000");
+ })
