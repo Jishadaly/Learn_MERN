b4b6af4c392339cbedb95cdace43d7cce8345953
jishadaly <jishadlm10@gmail.com>
2025-08-25 19:49:06 +0530

    Hash Table

--- boarding-week-one/DSA/hashTable.js
+++ boarding-week-one/DSA/hashTable.js
@@ -1,45 +1,119 @@
+// class HashTable {
+//     constructor(size) {
+//         this.table = new Array(size)
+//         this.size = size
+//     }
+
+//     hashFun(key) {
+//         let totel = 0;
+//         for (let i = 0; i < key.length; i++) {
+//             totel += key.charCodeAt(i)
+//         }
+
+//         return totel % this.size
+//     }
+
+//     set(key, value) {
+//         const index = this.hashFun(key)
+
+//         const bucket = this.table[index]
+//         if (!bucket) {
+//             this.table[index] = [[key, value]]
+//         } else {
+//             const sameKeyItem = bucket.find((val) => val[0] == key)
+//             if (sameKeyItem) {
+//                 sameKeyItem[1] = value
+//             } else {
+//                 bucket.push([key, value])
+//             }
+//         }
+//     }
+
+//     get(key) {
+//         const index = this.hashFun(key);
+//         const bucket = this.table[index]
+
+//         if (!bucket) {
+//             return undefined
+//         } else {
+//             const sameKeyItem = bucket.find((val) => val[0] == key)
+//             if (sameKeyItem) {
+//                 return sameKeyItem[1]
+//             } else {
+//                 return null
+//             }
+//         }
+//     }
+
+//     remove(key) {
+//         const index = this.hashFun(key)
+//         const bucket = this.table[index]
+
+//         if (!bucket) {
+//             return undefined
+//         } else {
+//             const sameKeyItem = bucket.find((val) => val[0] == key)
+
+//             if (sameKeyItem) {
+//                 bucket.splice(bucket.index(sameKeyItem), 1)
+//             }
+//         }
+//     }
+
+// }
+
+
+// const tabel = new HashTable(50);
+
+// tabel.set('jishad', 30)
+// console.log(tabel.get('jishad'));
+
+
 
 
 class HashTable {
     constructor(size) {
         this.table = new Array(size)
-        this.size = size
+        this.size = size;
     }
 
-    hashFun(key) {
-        let totel = 0;
+    hash(key) {
+        let total = 0;
         for (let i = 0; i < key.length; i++) {
-            totel += key.charCodeAt(i)
+            total += key.charCodeAt(i)
         }
-        
-        return totel % this.size
+
+        return total % this.size
     }
 
     set(key, value) {
-        const index = this.hashFun(key)
+        const index = this.hash(key);
+        const bucket = this.table[index];
 
-        const bucket = this.table[index]
+
         if (!bucket) {
-            this.table[index] = [[key, value]]
+            table.table[index] = [[key, value]]
         } else {
-            const sameKeyItem = bucket.find((val) => val[0] == key)
+            let sameKeyItem = bucket.find((item) => item[0] === key)
+
             if (sameKeyItem) {
                 sameKeyItem[1] = value
             } else {
                 bucket.push([key, value])
             }
         }
+
     }
 
     get(key) {
-        const index = this.hashFun(key);
-        const bucket = this.table[index]
-
+        const index = this.hash(key);
+        const bucket = this.table[index];
         if (!bucket) {
-            return undefined
+            return null
         } else {
-            const sameKeyItem = bucket.find((val) => val[0] == key)
+            let sameKeyItem = bucket.find((item) => item[0] === key)
             if (sameKeyItem) {
+
                 return sameKeyItem[1]
             } else {
                 return null
@@ -48,16 +122,17 @@ class HashTable {
     }
 
     remove(key) {
-        const index = this.hashFun(key)
+        const index = this.hash(key);
         const bucket = this.table[index]
 
-        if (!bucket) {
-            return undefined
-        } else {
-            const sameKeyItem = bucket.find((val) => val[0] == key)
+        if (bucket) {
+            let sameKeyItemIndex = bucket.findIndex((item) => item[0] === key)
+            console.log('index :' ,sameKeyItemIndex)
 
-            if (sameKeyItem) {
-                bucket.splice(bucket.index(sameKeyItem), 1)
+            if (sameKeyItemIndex) {
+                bucket.splice(sameKeyItemIndex, 1)
+            }else{
+                return null
             }
         }
     }
@@ -65,8 +140,10 @@ class HashTable {
 }
 
 
-const tabel = new HashTable(50);
-
-tabel.set('jishad', 30)
-console.log(tabel.get('jishad'));
+const table = new HashTable(50);
+table.set("name", "John")
+table.set("age", 25)
+console.log(table.get("name"))  // John
+table.remove("name")
+console.log(table.get("name"))  // null
 
--- boarding-week-one/DSA/queue.js
+++ boarding-week-one/DSA/queue.js
@@ -33,51 +33,66 @@
 // }
 
 
-class Node {
-   constructor(value){
-      this.head = value;
-      this.next = null
-   }
+// class Node {
+//    constructor(value){
+//       this.head = value;
+//       this.next = null
+//    }
 
    
-}
+// }
 
-class Queue {
+// class Queue {
+//    constructor(){
+//       this.front = null
+//       this.rear = null
+//       this.size = null
+//    }
+
+//    isEmpty() {
+//       return this.size === 0;
+//   }
+
+//    enqueue(value){
+//       const newNode = new Node(value)
+//       if (this.isEmpty()) {
+//          this.front = newNode
+//          this.rear = newNode
+//       }else{
+//          this.rear.next = newNode;
+//          this.rear = newNode
+//       }
+
+//       this.size ++
+//    }
+
+//    dequeue(){
+//       if (this.isEmpty()) {
+//          return null
+//       }
+
+//       let removedItem = this.front();
+//       this.front = this.front.next;
+
+//       if (!this.front) {
+//          this.rear = null  
+//       }
+
+//       this.size --
+//    }
+// }
+
+
+class Queue{
    constructor(){
-      this.front = null
-      this.rear = null
-      this.size = null
+      this.items = []
    }
-
-   isEmpty() {
-      return this.size === 0;
-  }
 
    enqueue(value){
-      const newNode = new Node(value)
-      if (this.isEmpty()) {
-         this.front = newNode
-         this.rear = newNode
-      }else{
-         this.rear.next = newNode;
-         this.rear = newNode
-      }
-
-      this.size ++
+      this.items.push(value)
    }
 
-   dequeue(){
-      if (this.isEmpty()) {
-         return null
-      }
-
-      let removedItem = this.front();
-      this.front = this.front.next;
-
-      if (!this.front) {
-         this.rear = null
-      }
-
-      this.size --
+   dequeue(value){
+      return this.items.shift(value);
    }
 }
