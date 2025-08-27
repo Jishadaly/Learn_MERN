92e4c9c92a703fbe292479ed9e48a957b1cc1b01
jishadaly <jishadlm10@gmail.com>
2025-08-27 18:21:35 +0530

    TRIE practice

--- DataStructure/trie/trie.js
+++ DataStructure/trie/trie.js
@@ -1,108 +1,85 @@
-class Node {
-  constructor() {
-    const children = {}
-    isWordEnd = false
-  }
-}
+// class Node {
+//   constructor() {
+//     const children = {}
+//     isWordEnd = false
+//   }
+// }
 
-class Trie {
-  constructor() {
-    this.root = new Node()
-  }
+// class Trie {
+//   constructor() {
+//     this.root = new Node()
+//   }
 
-  insert(word) {
-    let curr = this.root
-    for (let i = 0; i < word.length; i++) {
-      let charToInsert = word[i]
+//   insert(word) {
+//     let curr = this.root
+//     for (let i = 0; i < word.length; i++) {
+//       let charToInsert = word[i]
 
-      if (!curr.children.hasOwnProperty(charToInsert)) {
-        curr.children[charToInsert] = new Node()
-      }
+//       if (!curr.children.hasOwnProperty(charToInsert)) {
+//         curr.children[charToInsert] = new Node()
+//       }
 
-      curr = curr.children[charToInsert]
-    }
-    return curr.isWordEnd = true
-  }
+//       curr = curr.children[charToInsert]
+//     }
+//     return curr.isWordEnd = true
+//   }
 
-  contains(word) {
-    let curr = this.root
-    for (let i = 0; i < word.length; i++) {
-      let charToInsert = word[i]
-      if (!curr.children.hasOwnProperty(charToInsert)) {
-        return false
-      }
-      curr = curr.children[charToInsert]
-    }
-    return curr.isWordEnd
-  }
+//   contains(word) {
+//     let curr = this.root
+//     for (let i = 0; i < word.length; i++) {
+//       let charToInsert = word[i]
+//       if (!curr.children.hasOwnProperty(charToInsert)) {
+//         return false
+//       }
+//       curr = curr.children[charToInsert]
+//     }
+//     return curr.isWordEnd
+//   }
 
-  startWithPreffix(word) {
-    let curr = this.root
-    for (let i = 0; i < word.length; i++) {
-      let charToInsert = word[i]
-      if (!curr.children.hasOwnProperty(charToInsert)) {
-        return false
-      }
-      curr = curr.children[charToInsert]
-    }
-    return true
-  }
+//   startWithPreffix(word) {
+//     let curr = this.root
+//     for (let i = 0; i < word.length; i++) {
+//       let charToInsert = word[i]
+//       if (!curr.children.hasOwnProperty(charToInsert)) {
+//         return false
+//       }
+//       curr = curr.children[charToInsert]
+//     }
+//     return true
+//   }  
 
-  // delete(word){
-  //   if (!this.contains(word)) {
-  //     console.log(`the word "${word}" is not in the trie`);
-  //     return
-  //   }
+//   delete(word){
+//     if (!this.contains(word)) {
+//       console.log(`the word "${word}" is not in the trie`);
+//       return
+//     }
 
-  //   this.deleteHealper(this.root , word , 0)
-  // }
+//     this.deleteHealper(this.root , word , 0)
+//   }
 
-  // deleteHealper(node , word , index){
-  //    if (index === word.length ) {
-  //     node.isWordEnd = false
-  //     return
+//   deleteHealper(node , word , index){
+//      if (index === word.length ) {
+//       node.isWordEnd = false
+//       return
 
-  //    }
+//      }
 
-  //    const charToDelete = word[index]
-  //    const nextNode = node.children[charToDelete]
-  //    this.deleteHealper(nextNode , word ,index + 1)
+//      const charToDelete = word[index]
+//      const nextNode = node.children[charToDelete]
+//      this.deleteHealper(nextNode , word ,index + 1)
 
-  //    if (Object.keys(nextNode.children).length === 0 && !nextNode.isWordEnd) {
-  //       delete node.children[charToDelete]
-  //    }
-  // }
+//      if (Object.keys(nextNode.children).length === 0 && !nextNode.isWordEnd) {
+//         delete node.children[charToDelete]
+//      }
+//   }
 
-  delete(word) {
-    if (!this.contains(word)) {
-      console.log(`${word}  word not in the trie`);
-      return
-    }
-    this.deleteHelper(this.root, word, 0)
-  }
+// }  
 
-  deleteHelper(node, word, index) {
-    if (index === word.length) {
-      node.isWordEnd = false
-
-      return
-    }
-
-    const charToDelete = word[index]
-    const nextNode = node.children[charToDelete]
-    this.deleteHelper(nextNode, word, index + 1)
-
-    if (Object.keys(nextNode.children).length === 0 && !node.isWordEnd) {
-      delete node.children[charToDelete]
-    }
-  }
-
-}
 
 class Node {
   constructor() {
-    const children = {}
-    isWordEnd = false
+    this.children = {}
+    this.isWordEnd = false
   }
 }
 
@@ -110,19 +87,61 @@ class Trie {
   constructor() {
     this.root = new Node();
   }
-}
 
-insert(word){
-  let curr = this.root;
+  insert(word) {
+    let curr = this.root;
 
-  for (let i = 0; i < word.length; i++) {
-    let charToInsert = word[i]
+    console.log(curr)
 
-    if (!curr.children.hasOwnProperty(charToInsert)) {
-      curr.children[charToInsert] = new Node()
+    for (let i = 0; i < word.length; i++) {
+      let charToInsert = word[i];
+
+      if (!curr.children.hasOwnProperty(charToInsert)) {
+        curr.children[charToInsert] = new Node();
+      }
+      curr = curr.children[charToInsert]
     }
 
-    curr = curr.children[charToInsert]
+    curr.isWordEnd = true
   }
-  curr.isWordEnd = true
+
+  contains(word) {
+    let curr = this.root
+
+    for (let i = 0; i < word.length; i++) {
+      let charToFind = word[i]
+
+      if (!curr.children[charToFind]) {
+        return false
+      }
+      curr = curr.children[charToFind]
+    }
+
+    return curr.isWordEnd;
+  }
+
+  startWith(word) {
+    let curr = this.root
+
+    for (let i = 0; i < word.length; i++) {
+      let wordToFind = word[i]
+
+      if (!curr.children.hasOwnProperty(wordToFind)) {
+        return false
+      }
+
+      curr = curr.children[wordToFind]
+    }
+
+    return true
+  }
+
 }
+
+const trie = new Trie()
+
+trie.insert('hello world')
+console.log(trie.contains('hello world'))
+console.log(trie.startWith('hello'))
+
+// console.log(trie.root.children)
